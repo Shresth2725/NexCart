0b8c413de937262cc4021f1f4666504dd63f4c24
Shresth <2725shresth@gmail.com>
2026-05-12 00:54:41 +0530

    fix: update fetchCart to correctly extract cart data from response object

--- frontend/src/store/slices/cartSlice.js
+++ frontend/src/store/slices/cartSlice.js
@@ -3,7 +3,7 @@ import api from '../../api/axios';
 
 export const fetchCart = createAsyncThunk('cart/fetchCart', async () => {
   const response = await api.get('/cart/cart/');
-  return response.data;
+  return response.data.cart;
 });
 
 const initialState = {
